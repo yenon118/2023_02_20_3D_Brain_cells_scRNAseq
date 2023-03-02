498973da325da97e17757590c10daa54f0df95dc
Yen On Chan <yenon118@gmail.com>
2023-03-01 18:21:49 -0600

    Added annotation to DoHeatmap datasets

--- 2023_02_23_Seurat_OSA_NG/scripts/2023_02_28_doheatmap.R
+++ 2023_02_23_Seurat_OSA_NG/scripts/2023_02_28_doheatmap.R
@@ -14,12 +14,34 @@ library(Matrix)
 library(Seurat)
 library(patchwork)
 
+library(biomaRt)
+
 
 ##################################################
 # Constants/Variables
 ##################################################
 
-n <- 5
+
+##################################################
+# Argparse
+##################################################
+
+parser <- argparse::ArgumentParser()
+
+parser$add_argument("-n", "--num", type="integer", help="Top number for slice")
+
+args <- parser$parse_args()
+
+n <- tryCatch({
+	args$num
+}, error = function(e) {
+	return(NULL)
+})
+
+if (is.null(n)) {
+	message("n is NULL!!!")
+	quit(status=1)
+}
 
 
 ##################################################
@@ -33,6 +55,38 @@ if(!dir.exists(output_path)){
 	if(!dir.exists(output_path)){
 		quit(status=1)
 	}
+}
+
+
+##################################################
+# Query annotations from biomaRt
+##################################################
+
+getBMTable <- function(values, dataset) {
+	ensembl <- useEnsembl(biomart = "genes", dataset = dataset)
+
+	attributes = listAttributes(ensembl)
+
+	# head(searchAttributes(mart = ensembl, pattern = "id"))
+	# head(searchFilters(mart = ensembl, pattern = "id"))
+
+	bm_table <- getBM(
+		attributes = c(
+			'ensembl_gene_id',
+			'external_gene_name',
+			'chromosome_name',
+			'start_position',
+			'end_position',
+			'strand',
+			'gene_biotype',
+			'description'
+		),
+		filters = 'external_gene_name',
+		values = values,
+		mart = ensembl
+	)
+
+	return(bm_table)
 }
 
 
@@ -53,62 +107,98 @@ DefaultAssay(dat) <- "RNA"
 
 
 libraries <- c(
-    "seurat_clusters",
-    "HumanPrimaryCellAtlasData",
-    "BlueprintEncodeData",
-    "MouseRNAseqData",
-    "ImmGenData",
-    "DatabaseImmuneCellExpressionData",
-    "NovershternHematopoieticData",
-    "MonacoImmuneData",
-    "Experiment"
+	"seurat_clusters",
+	"HumanPrimaryCellAtlasData",
+	"BlueprintEncodeData",
+	"MouseRNAseqData",
+	"ImmGenData",
+	"DatabaseImmuneCellExpressionData",
+	"NovershternHematopoieticData",
+	"MonacoImmuneData",
+	"Experiment"
 )
 
 
 for (i in 1:length(libraries)) {
 
-    data_markers <- tryCatch({
-        read.table(
-            file = file.path("../output/Markers", paste0("data_markers_", libraries[i], ".txt")),
-            sep = "\t",
-            header = TRUE,
-            check.names = FALSE,
-            stringsAsFactors = FALSE
-        )
-    }, error = function(e) {
-        return(NULL)
-    })
+	fp <- file.path("../output/Markers", paste0("data_markers_", libraries[i], ".txt"))
 
-    if(!is.null(data_markers)) {
-        if(nrow(data_markers) > 0 & ncol(data_markers) > 0) {
-            top_n_markers <- data_markers %>%
-                group_by(cluster) %>%
-                top_n(n = n, wt = avg_log2FC)
+	if (file.exists(fp)) {
 
-            p <- DoHeatmap(
-                dat,
-                features = top_n_markers$gene,
-                group.by = libraries[i],
-                label = ifelse(libraries[i] == "seurat_clusters", TRUE, FALSE)
-            )
+		data_markers <- tryCatch({
+			read.table(
+				file = fp,
+				sep = "\t",
+				header = TRUE,
+				check.names = FALSE,
+				stringsAsFactors = FALSE
+			)
+		}, error = function(e) {
+			return(NULL)
+		})
 
-            ggsave(
-                filename = paste0("doheatmap_", n, "_", libraries[i], ".png"),
-                plot = p,
-                path = output_path,
-                width = 14,
-                height = 10
-            )
+		if(!is.null(data_markers)) {
+			if(nrow(data_markers) > 0 & ncol(data_markers) > 0) {
+				top_n_markers <- data_markers %>%
+					group_by(cluster) %>%
+					top_n(n = n, wt = avg_log2FC)
 
-            write.table(
-                x = top_n_markers,
-                file = file.path(output_path, paste0("doheatmap_", n, "_", libraries[i], ".txt")),
-                sep = "\t",
-                na = "",
-                quote = FALSE,
-                row.names = FALSE
-            )
-        }
-    }
+				p <- DoHeatmap(
+					dat,
+					features = top_n_markers$gene,
+					group.by = libraries[i],
+					label = ifelse(libraries[i] == "seurat_clusters", TRUE, FALSE)
+				)
+
+				ggsave(
+					filename = paste0("doheatmap_", n, "_", libraries[i], ".png"),
+					plot = p,
+					path = output_path,
+					width = 14,
+					height = 10
+				)
+
+				write.table(
+					x = top_n_markers,
+					file = file.path(output_path, paste0("doheatmap_", n, "_", libraries[i], ".txt")),
+					sep = "\t",
+					na = "",
+					quote = FALSE,
+					row.names = FALSE
+				)
+
+				bm_table <- out <- tryCatch({
+					getBMTable(values = unique(sort(top_n_markers$gene)), dataset = "hsapiens_gene_ensembl")
+				}, error=function(e) {
+					message(e)
+					return(NULL)
+				}, warning=function(e) {
+					message(e)
+					return(NULL)
+				}, finally=function(e) {
+					message(e)
+				})
+
+				if (!is.null(bm_table)) {
+					top_n_markers$external_gene_name <- top_n_markers$gene
+
+					annotated_top_n_markers <- top_n_markers %>%
+						left_join(bm_table, by = "external_gene_name") %>%
+						as.data.frame(check.names = FALSE, stringsAsFactors = FALSE)
+
+					write.table(
+						x = annotated_top_n_markers,
+						file = file.path(output_path, paste0("doheatmap_", n, "_", libraries[i], "_annotated.txt")),
+						sep = "\t",
+						na = "",
+						quote = FALSE,
+						row.names = FALSE
+					)
+				}
+
+			}
+		}
+
+	}
 
 }
